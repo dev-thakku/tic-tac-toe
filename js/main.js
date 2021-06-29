ad8cf8e3000c2c50d507ccc1ee39c6e3049a2694
dev-thakku <jemshithtk@gmail.com>
2021-06-29 22:49:18 +0530

    ✅Completed Board

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 let startButton = document.querySelector(".btn-primary");
-let board = document.querySelector(".board-wrapper");
+let closeButton = document.querySelector(".close-button");
+let board = document.querySelector(".board-container");
+
+const xTile = '<div class="mark x">X</div>';
+const oTile = '<div class="mark o">O</div>';
 
 startButton.addEventListener("click", (e) => {
     e.preventDefault();
     board.classList.add("show");
 })
+
+closeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(board);
+    board.classList.remove("show");
+})
+
+document.querySelector('.tile[data-index="1"]').innerHTML = oTile;
